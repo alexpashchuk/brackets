542c9f0ea4e9f255810a21e889e45c82c541d546
Aleksey Pashchuk <aleksey.pashchuk@gmail.com>
2023-01-17 19:48:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const bracketsPairs = bracketsConfig.reduce((acc, elem) => {
+        return { ...acc, [elem[1]]: elem[0] }
+    }, {})
+
+    const stack = []
+
+    for (let i = 0; i < str.length; i++) {
+        const currentSymbol = str[i]
+
+        if (stack.length && stack[stack.length - 1] === bracketsPairs[currentSymbol]) {
+            stack.pop()
+        } else {
+            stack.push(currentSymbol)
+        }
+    }
+
+    return stack.length === 0
+
 }
